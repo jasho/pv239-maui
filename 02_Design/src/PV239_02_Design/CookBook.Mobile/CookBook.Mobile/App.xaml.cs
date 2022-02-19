9918fdbd22dfe8eb5cdad40b1edb48d387e438f0
jasho <roman.jasek@hotmail.com>
2022-02-19 08:32:52 +0100

    Added sample code for lab 02

---
+++ 02_Design/src/PV239_02_Design/CookBook.Mobile/CookBook.Mobile/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using CookBook.Mobile.Views;
+using Xamarin.Forms;
+
+namespace CookBook.Mobile
+{
+    public partial class App
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainView());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ 02_Design/src/PV239_02_Design/CookBook.Mobile/CookBook.Mobile/Views/MainView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CookBook.Mobile.Views
+{
+    public partial class MainView
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        private void IngredientsButton_OnClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new IngredientDetailView());
+        }
+    }
+}
