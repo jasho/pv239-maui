0e5888cd34275cc03659497f6276022a1076547e
jasho <roman.jasek@hotmail.com>
2025-03-07 08:31:07 +0100

    Add TmpViewModel for manual binding and command sample

---
+++ 03_MVVM/src/PV239_03_MVVM/CookBook.Maui/ViewModels/TmpViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using CookBook.Maui.Models;
+
+namespace CookBook.Maui.ViewModels;
+
+public class TmpViewModel : INotifyPropertyChanged
+{
+    public IngredientDetailModel? Ingredient
+    {
+        get;
+        set
+        {
+            if (Equals(value, field))
+            {
+                return;
+            }
+
+            field = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public ICommand DoStuffCommand { get; set; }
+
+    public TmpViewModel()
+    {
+        DoStuffCommand = new Command(DoStuff, CanDoStuff);
+    }
+
+    private void DoStuff()
+    {
+    }
+
+    private bool CanDoStuff()
+    {
+        return true;
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+
+}
